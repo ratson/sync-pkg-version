6c2dd63cf5e5c23b8c45077bf96cdd5deadc4b31
Ratson <github@ratson.name>
2016-09-09 11:21:11 +0800

    Return promise

--- src/index.js
+++ src/index.js
@@ -2,24 +2,33 @@ import fs from 'fs'
 
 export function updateCordovaPluginVersion(filename, version) {
   const versionRE = /<plugin [\s\S]*? version=\s*["']?([^\s'"]+)/m
-  fs.readFile(filename, 'utf8', (err, data) => {
-    if (err) {
-      throw err
-    }
-    const match = versionRE.exec(data)
-    if (!match) {
-      throw new Error(`cannot find version property in ${filename}`)
-    }
-    if (match[1] === version) {
-      console.log(`${filename}\t${match[1]}`)
-      return
-    }
-    data = data.substring(0, match.index + match[0].length - match[1].length) + version + data.substring(match.index + match[0].length)
-    fs.writeFile(filename, data, 'utf8', (err) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, 'utf8', (err, data) => {
       if (err) {
-        throw err
+        return reject(err)
       }
-      console.log(`${filename}\t${match[1]} -> ${version}`)
+      const match = versionRE.exec(data)
+      if (!match) {
+        return reject(new Error(`cannot find version property in ${filename}`))
+      }
+      if (match[1] === version) {
+        console.log(`${filename}\t${match[1]}`)
+        return resolve({
+          before: match[1],
+          after: version,
+        })
+      }
+      data = data.substring(0, match.index + match[0].length - match[1].length) + version + data.substring(match.index + match[0].length)
+      fs.writeFile(filename, data, 'utf8', (err) => {
+        if (err) {
+          return reject(err)
+        }
+        console.log(`${filename}\t${match[1]} -> ${version}`)
+        return resolve({
+          before: match[1],
+          after: version,
+        })
+      })
     })
   })
 }
