ae9e0056dc643a75c1bc2f92cdc18c7db58e5d91
Ratson <github@ratson.name>
2017-07-21 18:40:30 +0800

    Add ESLint

--- test/test.js
+++ test/test.js
@@ -3,13 +3,12 @@ import fs from 'fs'
 import tempWrite from 'temp-write'
 import test from 'ava'
 
-import {updateCordovaPluginVersion} from '../lib'
+import { updateCordovaPluginVersion } from '../lib'
 
 test(async t => {
   const pluginXml = `${__dirname}/fixtures/plugin.xml`
   const filename = await tempWrite(fs.readFileSync(pluginXml, 'utf8'))
-  return updateCordovaPluginVersion(filename, '0.0.1').then(({before, after}) => {
-    t.is(before, '0.0.0')
-    t.is(after, '0.0.1')
-  })
+  const { before, after } = await updateCordovaPluginVersion(filename, '0.0.1')
+  t.is(before, '0.0.0')
+  t.is(after, '0.0.1')
 })
