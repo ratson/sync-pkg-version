ed0987dc246dcc11b137bb6a824decac14d02a09
Ratson <github@ratson.name>
2021-10-13 14:07:20 +0800

    Format and lint using deno

--- test/test.js
+++ test/test.js
@@ -1,23 +1,23 @@
-import fse from 'fs-extra'
-import tempWrite from 'temp-write'
-import { dirname, join } from 'path'
-import { updateCordovaPluginVersion } from '..'
-import { fileURLToPath } from 'url'
+import fse from "fs-extra";
+import tempWrite from "temp-write";
+import { dirname, join } from "path";
+import { updateCordovaPluginVersion } from "..";
+import { fileURLToPath } from "url";
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-test('updateCordovaPluginVersion', async () => {
-  const pluginXml = join(__dirname, 'fixtures/plugin.xml')
-  const filename = await tempWrite(await fse.readFile(pluginXml, 'utf8'))
-  const { before, after } = await updateCordovaPluginVersion(filename, '0.0.1')
-  expect(before).toBe('0.0.0')
-  expect(after).toBe('0.0.1')
-})
+test("updateCordovaPluginVersion", async () => {
+  const pluginXml = join(__dirname, "fixtures/plugin.xml");
+  const filename = await tempWrite(await fse.readFile(pluginXml, "utf8"));
+  const { before, after } = await updateCordovaPluginVersion(filename, "0.0.1");
+  expect(before).toBe("0.0.0");
+  expect(after).toBe("0.0.1");
+});
 
-test('tab', async () => {
-  const pluginXml = join(__dirname, 'fixtures/plugin-tab.xml')
-  const filename = await tempWrite(await fse.readFile(pluginXml, 'utf8'))
-  const { before, after } = await updateCordovaPluginVersion(filename, '0.0.1')
-  expect(before).toBe('0.0.0')
-  expect(after).toBe('0.0.1')
-})
+test("tab", async () => {
+  const pluginXml = join(__dirname, "fixtures/plugin-tab.xml");
+  const filename = await tempWrite(await fse.readFile(pluginXml, "utf8"));
+  const { before, after } = await updateCordovaPluginVersion(filename, "0.0.1");
+  expect(before).toBe("0.0.0");
+  expect(after).toBe("0.0.1");
+});
